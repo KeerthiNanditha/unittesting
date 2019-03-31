0627d152599b046808ad5b3f0d51f76ea14cc4de
KeerthiNanditha <36481728+KeerthiNanditha@users.noreply.github.com>
2019-03-31 16:02:06 +0530

    Create calci.c

---
+++ calci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include "calci.h"
+ 
+
+
+int addition(int a, int b)
+{
+    
+    return (a+b);
+}
+ 
+
+int mod(int a,int b)
+{
+ if(a>=0&&b>=0)
+ {
+  return a%b;
+ }
+ else
+ {
+  return 0;
+ }
+ }
